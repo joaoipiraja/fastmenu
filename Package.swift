6f77bbffa0cafdff1f8e7085cb76493364f773ee
João Victor Ipirajá de Alencar <joaovictorpiraja@gmail.com>
2022-03-11 11:54:25 -0300

    Update project

--- Package.swift
+++ Package.swift
@@ -6,9 +6,7 @@ import PackageDescription
 let package = Package(
     name: "fastmenu",
     dependencies: [
-        
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.4.0"),
-        
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
