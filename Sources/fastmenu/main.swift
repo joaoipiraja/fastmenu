17076d98fec4ce1444b31ac536084f23b364ff86
João Victor Ipirajá de Alencar <joaovictorpiraja@gmail.com>
2022-03-14 11:54:12 -0300

    Add Core ML model class to main

--- Sources/fastmenu/main.swift
+++ Sources/fastmenu/main.swift
@@ -1,1 +1,25 @@
+import CoreML
+import ConsoleKit
 
+let model: food_classifier? = {
+    do {
+        let config = MLModelConfiguration()
+        return try food_classifier(configuration: config)
+    }catch{
+        Console.writeError("Could not open the model")
+        return nil
+    }
+}()
+
+Console.write("Output >> ")
+
+if let model_aux = model{
+if let menu_output = Console.readLine(){
+    guard let food_label = try? model_aux.prediction(text: menu_output) else{
+        Console.writeError("Could not predict ouput")
+        fatalError("Unexpected runtime error.")
+    }
+    
+    Console.writeLine(food_label.label)
+}
+}
