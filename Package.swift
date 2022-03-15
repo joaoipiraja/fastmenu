99bc569f680f4959b6998decca781dc4f65197c8
João Victor Ipirajá de Alencar <joaovictorpiraja@gmail.com>
2022-03-15 07:48:29 -0300

    Solve CoreML model compilation bug

--- Package.swift
+++ Package.swift
@@ -5,18 +5,26 @@ import PackageDescription
 
 let package = Package(
     name: "fastmenu",
+    platforms: [
+           .macOS(.v10_15)
+       ],
+    
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.4.0"),
-        .package(url: "https://github.com/apparata/ConsoleKit.git", from: "0.2.1")
+        .package(url: "https://github.com/apparata/ConsoleKit.git", from: "0.2.0")
     ],
+ 
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "fastmenu",
             dependencies: [
+                .product(name: "ConsoleKit", package: "ConsoleKit"),
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
-            ]),
+            ],
+            resources: [.process("food_classifier.mlmodelc")]
+        ),
         .testTarget(
             name: "fastmenuTests",
             dependencies: ["fastmenu"]),
