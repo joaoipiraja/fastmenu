1b15b3f1a37044994998722333d5dccf5e43c940
João Victor Ipirajá de Alencar <joaovictorpiraja@gmail.com>
2022-03-22 11:10:19 -0300

    Update main to Swift Argument Passer Pattern

--- Sources/fastmenu/main.swift
+++ Sources/fastmenu/main.swift
@@ -1,13 +1,63 @@
-import CoreML
 import ConsoleKit
+import ArgumentParser
 
 
+//swift run fastmenu --service-time "12:30 ~ 13:00" --copy-to-paste-board
 
-Console.write("Digite o cardápio >> ")
+extension FastMenu{
+    
+    mutating func run() throws{
+       let menu = Menu(serviceTime: self.serviceTime)
+        
+        Console.print("\(.brightRed)Digite o cardápio >>\(.reset)")
 
-if let input = Console.readLine(){
-    Console.writeLine(Menu(menuInput: input).description)
+        while(true){
+            if let input = Console.readLine(), !input.isEmpty{
+                menu.split_menu(menuInput: input)
+            }else{
+                break
+            }
+            
+            Console.print("\(.brightRed)Digite ENTER para sair >>\(.reset)")
+        }
+        
+        
+        if copyToPasteBoard{
+            Console.print("\(.green)\(.bold)\n-----COPIED TO PASTEBOARD------\n\(.reset)")
+            menu.description.copyToPasteboard()
+        }else{
+            Console.print("\n-----------\n")
+        }
+        
+    
+        
+        Console.writeLine(menu.description)
+    }
+
+    
 }
+
+
+struct FastMenu: ParsableCommand{
+    
+    static var configuration = CommandConfiguration(
+            abstract: "Generate your menus easier than ever",
+            version: "1.0.0"
+    )
+    
+
+    @Flag(help: "Execute menu script")
+    var copyToPasteBoard = false
+    
+    @Option(help: "Time interval of service")
+    var serviceTime = "11:30 ~ 12:30"
+        
+}
+
+FastMenu.main()
+
+
+
 
 
     
